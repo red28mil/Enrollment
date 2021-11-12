1d379d08f584d68b07b25443654f6f5f9e48b0a8
Maeliosa Leamy <59532461+red28mil@users.noreply.github.com>
2021-11-12 23:21:42 +0000

    first

---
+++ index.js
@@ -0,0 +1,33 @@
+const PORT  = 8000
+const express = require('express')
+const axios = require ('axios')
+const cheerio = require('cheerio')
+
+const app = express()
+
+const articles = []
+
+app.get ('/',(req,res ) => {
+    res.json("welcome")
+
+})
+app.get('/news',(req,res) => {
+    axios.get('https://www.theguardian.com/environment/climate-crsis')
+        .then((response ) => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                articles.push({
+                    title,
+                    url
+                })
+
+            })
+            res.json(articles)
+
+        }).catch((err) => console.log(err))
+
+})
+app.listen(PORT, () => console.log('server running port'))
